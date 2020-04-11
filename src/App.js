99f3ca91647bcf59fe53d94654863f1d182a6b16
bob <mandrecomunicacion@gmail.com>
2020-04-11 18:13:10 -0500

    hola

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 import { Routers } from './components/Routers'
 import { MsgContext } from './contexts/MsgContext'
@@ -7,3 +8,31 @@ export const App = () => (
         <Routers />
     </MsgContext.Provider>
 )
+=======
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> Initialize project using Create React App
